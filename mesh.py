5133b1bec93de28f17f5cd903d015e10f2fa8aa5
kinda-cookie-monster <88910337+kinda-cookie-monster@users.noreply.github.com>
2021-09-01 02:12:43 +0300

    Поддержка LaTeX формул, точное построение строчек
    - Была восстановлена поддержка LaTeX формул для заданий по математике. Теперь вместо пустоты будут отображаться модифицированные и более понятные интерпретации формул, и на случай если нет замены, просто вставляет формулу. Для этих целей была добавлена функция convert_latex(). Дополнительные LaTeX формулы, оптимизации планируются позже.
    
    - Было изменено способ построения условия задания и ответов на него. Теперь в строки автоматически вставляются дополнительные элементы, которые до этого просто не отображались. За это отвечает функция generate_string(), которая получает словарь с информацией о строении текста условия/ответа и возращает модифицированную строку.
    
    - Небольшие изменения в названиях переменных и небольшая уборка кода (хотя я сам не знаю, делал ли я её или нет...)

--- mesh.py
+++ mesh.py
@@ -1,4 +1,4 @@
-import requests, json, hashlib
+import re, requests, json, hashlib
 
 def auth (demo = True, login = "", password = ""):
     if demo:
@@ -55,138 +55,190 @@ def fetch_json (auth_data, test_variant, test_type):
         headers = {"Content-type": "application/json"}      
     )
     
-    return json.loads(task_responce.text)["training_tasks"]
+    return json.loads(task_responce.text)
 
 
-def get_answers (mesh_url):
-    answers = []
-    broken_types = []
+def convert_latex (string):
+    string = string.replace("\\", "").replace("cdot", "*")
+
+    simple_transforms = {
+        "\^circ"       : ["^circ", " градусов"],
+        "bigtriangleup": ["bigtriangleup", "треугольник"],
+        "angle"        : ["angle", "/_"],
+    }
     
-    auth_data = auth()
-    test_variant = get_variant(mesh_url)
-    test_type = get_type(mesh_url)
+    for regex, changes in simple_transforms.items():
+        index = re.compile(regex)
 
-    task_contents = fetch_json(auth_data, test_variant, test_type)
+        for _ in index.findall(string): 
+            string = string.replace(changes [0], changes [1])
     
-    for question in task_contents:
-        question_text = ""
-        final_answer = ""
 
-        answer_info = question["test_task"]["answer"]
-        answer_type = answer_info["type"]
+    fraction = re.compile("dfrac{(.*?)}{(.*?)}")
+    square_root = re.compile("sqrt{(.*?)}")
+    power = re.compile("(.*?)\^(.*)")
 
-        # Составляем условие задания
-        for que_entry in question["test_task"]["question_elements"]:
-            if que_entry["type"] == "content/text":
-                question_text += que_entry["text"] + " "
+    for i in fraction.findall(string):
+        string = string.replace("dfrac{" + str(i[0]) + "}{" + str(i[1]) + "}", str(i[0]) + "/" + str(i[1]))
+    
+    for i in square_root.findall(string):
+        string = string.replace("sqrt{" + str(i) + "}", "Корень из " + str(i))
+    
+    for i in power.findall(string):
+        string = string.replace(str(i[0]) + "^" + str(i[1]), str(i[0]) + " в степени " + str(i[1]))
+    
+    return string
 
-            elif que_entry["type"] == "content/atomic":
-                if que_entry["atomic_type"] == "image": 
-                    url = f'(https://uchebnik.mos.ru/cms{que_entry["preview_url"]})'
+
+def generate_string (string_data):
+    try:
+        text = string_data ["text"]
+        options = []
+
+        move_point = 0
+
+        for option in string_data ["content"]:
+            insert_index = option ["position"] + move_point
+            text = text [0:insert_index] + "{}" + text [insert_index:]
+            move_point += 2
+
+            option_type = option ["type"]
+
+            if option_type == "content/math":
+                option_text = convert_latex(option ["content"])
+            else:
+                option_text = option ["content"]
+
+            options.append(f" {option_text} ")
+
+        return text.format(*options)
+    
+    except KeyError:
+        if string_data ["atomic_type"] == "image": 
+            return f'(https://uchebnik.mos.ru/cms{string_data ["preview_url"]})'
                 
-                elif que_entry["atomic_type"] == "video": 
-                    url = f'({que_entry["preview_url"]})'
+        elif string_data ["atomic_type"] == "video": 
+            return f'({string_data ["preview_url"]})'
 
-                question_text += f"\n{url}"
 
-        # Просматриваем каждый вид заданий и находим верный ответ
+def get_answers (url):
+    answers = []
+    borked = []
+
+    auth_data = auth()
+    task_variant = get_variant(url)
+    task_type = get_type(url)
+
+    task_answers = fetch_json(auth_data, task_variant, task_type)
+
+    for exercise in task_answers ["training_tasks"]:
+        statement = ""
+        answer = ""
+
+        question_data = exercise ["test_task"] ["question_elements"]
+        answer_data   = exercise ["test_task"] ["answer"]
+        answer_type   = answer_data ["type"]
+
+        for string_chunk in question_data:
+            statement += generate_string(string_chunk)
+
         if answer_type == "answer/single":
-            answer_id = answer_info["right_answer"]["id"]
+            answer_id = answer_data ["right_answer"] ["id"]
             
-            for entry in answer_info["options"]:
-                if entry["id"] == answer_id: 
-                    final_answer = entry["text"]
+            for entry in answer_data ["options"]:
+                if entry ["id"] == answer_id: 
+                    answer = generate_string(entry)
 
         
         elif answer_type == "answer/string": 
-            final_answer = answer_info["right_answer"]["string"]
+            answer = generate_string(answer_data ["right_answer"])
         
         
         elif answer_type == "answer/order":
-            order_ids = answer_info["right_answer"]["ids_order"]
+            order_ids = answer_data["right_answer"]["ids_order"]
 
             for correct_order_element in order_ids:
-                for answer_entry in answer_info["options"]:
+                for answer_entry in answer_data["options"]:
                     if answer_entry["id"] == correct_order_element:
-                        final_answer += answer_entry["text"] + ", "
+                        answer += generate_string(answer_entry) + ", "
 
 
         elif answer_type == "answer/groups":
-            correct_groups = answer_info["right_answer"]["groups"]
+            correct_groups = answer_data["right_answer"]["groups"]
 
             for group in correct_groups:
                 group_name = ""
                 group_elements = ""
 
-                for answer_entry in answer_info["options"]:
+                for answer_entry in answer_data["options"]:
                     if answer_entry["id"] in group["options_ids"]:
-                        group_elements += answer_entry["text"] + ",\n\t"
+                        group_elements += generate_string(answer_entry) + ",\n\t"
 
                     elif answer_entry["id"] == group["group_id"]: 
-                        group_name = answer_entry["text"]
+                        group_name = generate_string(answer_entry)
 
-                final_answer += f"{group_name}:\n\t{group_elements}"
+                answer += f"{group_name}:\n\t{group_elements}"
             
-            final_answer = final_answer[:-2]
+            answer = answer[:-2]
 
 
         elif answer_type == "answer/multiple":
-            answer_ids = answer_info["right_answer"]["ids"]
+            answer_ids = answer_data["right_answer"]["ids"]
 
             for answer_id in answer_ids:
-                for answer_entry in answer_info["options"]:
+                for answer_entry in answer_data["options"]:
                     if answer_entry["id"] == answer_id:
-                        final_answer += f"{answer_entry['text']}; "
+                        answer += f"{generate_string(answer_entry)}; "
 
-            final_answer = final_answer[:-2]
+            answer = answer[:-2]
 
 
         elif answer_type == "answer/inline/choice/single":
-            answer_ids = answer_info["right_answer"]["text_position_answer"]
+            answer_ids = answer_data["right_answer"]["text_position_answer"]
 
             for field_num, answer_id in enumerate(answer_ids):
-                entry_options = answer_info["text_position"][field_num]["options"]
+                entry_options = answer_data["text_position"][field_num]["options"]
                 
                 for entry in entry_options:
                     if entry["id"] == answer_id["id"]:
-                        final_answer += f"{entry['text']}; "
+                        answer += f"{generate_string(entry)}; "
 
-            final_answer = final_answer[:-2]
+            answer = answer[:-2]
 
 
         elif answer_type == "answer/number":
-            final_answer = str(answer_info["right_answer"]["number"])
+            answer = str(answer_data["right_answer"]["number"])
 
 
         elif answer_type == "answer/match":
-            correct_elements = answer_info["right_answer"]["match"]
+            correct_elements = answer_data["right_answer"]["match"]
 
             for key, value in correct_elements.items():
                 key_name = ""
                 value_name = ""
 
-                for answer_entry in answer_info["options"]:
+                for answer_entry in answer_data["options"]:
                     if answer_entry["id"] == key: 
-                        key_name = answer_entry["text"]
+                        key_name = generate_string(answer_entry)
                     elif answer_entry["id"] == value[0]: 
-                        value_name = answer_entry["text"]
+                        value_name = generate_string(answer_entry)
 
-                final_answer += f" \n{key_name}: {value_name}"
+                answer += f" \n{key_name}: {value_name}"
 
 
         elif answer_type == "answer/gap/match/text":
-            answer_ids = answer_info["right_answer"]["text_position_answer"]
+            answer_ids = answer_data["right_answer"]["text_position_answer"]
 
             for answer_id in answer_ids:
-                for answer_option in answer_info["options"]:
+                for answer_option in answer_data["options"]:
                     if answer_id["id"] == answer_option["id"]: 
-                        final_answer += f"{answer_option['text']}; "
+                        answer += f"{generate_string(answer_option)}; "
 
-            final_answer = final_answer[:-2]
+            answer = answer[:-2]
 
-        if final_answer:
-            answers.append([question_text, final_answer])
         else:
-            broken_types.append(answer_type)
+            borked.append([answer_type], question_data, answer_data)
 
-    return answers, broken_types
+        answers.append([statement, answer])
+
+    return answers, borked
