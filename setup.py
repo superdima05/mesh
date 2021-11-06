ecfe7001e5d8e635f7b2ec90f5ed6baaf81abf19
zsaz <dmitriy@pimkin.cc>
2021-11-07 01:24:07 +0300

    README.md fix

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import find_packages, setup
 
+with open("README.md") as f:
+	long_description = f.read()
+
 setup(
-    name='mesh',
+    name='libmesh',
     packages=find_packages(),
-    version='0.0.5',
+    version='0.0.6',
     description='This library can get answers from uchebnik.mos.ru ',
     author='superdima05, kinda-cookie-monster',
     python_requires='>=3.9',
     scripts=['scripts/meshLauncher'],
     license='GPL-3.0',
     license_file='LICENSE',
-    install_requires=['requests']    
+    install_requires=['requests'],
+    long_description_content_type='text/markdown',
+    long_description=long_description    
 )
