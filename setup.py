4f20314437141288117e36e9ffc37fc2aff49f68
zsaz <dmitriy@pimkin.cc>
2021-11-07 00:45:37 +0300

    Now mesh is library!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mesh',
+    packages=find_packages(),
+    version='0.0.5',
+    description='This library can get answers from uchebnik.mos.ru ',
+    author='superdima05, kinda-cookie-monster',
+    license='GPL-3.0',
+)
